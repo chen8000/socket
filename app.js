ec45f7fe5148284ef1d3b1abe618a3735400bbd3
chen8000 <13126618000@163.com>
2018-08-08 19:19:31 +0800

    建立服务器和客户端的连接，及互相订阅和监听数据

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const http = require('http');
+const fs = require('fs');
+
+const app = http.createServer(function(req, res){
+    fs.readFile('index.html', (err, data) => {
+        if(err) return 
+        res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+        res.end(data)
+    })
+})
+
+const io = require('socket.io')(app);
+
+io.on('connection', (socket) => {
+    console.log('建立连接了')
+    socket.on('ww', (data) => {
+        console.log(data)
+
+        socket.emit('aa','我是服务器的数据')
+    })
+
+})
+
+app.listen(8000);
+
+
+
