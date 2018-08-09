d209c8dafb894d27bba8733d05e287658adf3ffa
chen8000 <13126618000@163.com>
2018-08-09 16:48:31 +0800

    订阅监听，群发

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ const app = http.createServer(function(req, res){
 
 const io = require('socket.io')(app);
 
+// 建立连接
 io.on('connection', (socket) => {
     console.log('建立连接了')
     socket.on('ww', (data) => {
         console.log(data)
 
-        socket.emit('aa','我是服务器的数据')
+        // socket.emit('aa','我是服务器的数据')  /* 谁发给我，我回给谁 */
+        // io.emit('aa','都有') /* 不管谁发我，我都告诉所有人 */
     })
 
 })
