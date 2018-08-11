a42e43ad145687057cbb3ff3c3db4e4c3cde403d
chen8000 <13126618000@163.com>
2018-08-11 19:36:32 +0800

    koa使用socket

---
+++ koa_socket/app.js
@@ -0,0 +1,60 @@
+
+
+
+
+
+// art-template    腾讯开发的
+
+const Koa = require('koa');
+const router = require('koa-router')();
+const render = require('koa-art-template');
+const path = require('path');
+
+const app = new Koa();
+
+//配置 koa-art-template
+render(app, {
+    root : path.join(__dirname, 'views'), // 视图引擎的位置
+    extname : '.html',  // 后缀名
+    debug : process.env.NODE_ENV !== 'production'  // 是否开启调试模式
+});
+
+
+
+
+
+router.get('/', async (ctx) => {
+
+    ctx.render('index')
+});
+
+
+//启动路由
+app
+    .use(router.routes()) // 启动路由
+    .use(router.allowedMethods()); //  根据上下文环境配置 response 响应头
+
+app.listen(8000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
