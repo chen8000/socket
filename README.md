d91be5ba034e388eb1e80a72748db4a9714faf12
chen8000 <13126618000@163.com>
2018-08-11 19:02:14 +0800

    多人群聊

--- README.md
+++ README.md
@@ -34,3 +34,47 @@
     socket.on('aa', (data) => {
         console.log(data)
     })
+
+## 多人
+
+客户端发布
+* socket.emit('msg', {data})
+
+客户端监听
+* socket.on('msgg', (data) => { console.log(data) })
+
+服务器端发布
+* socket.emit('msgg', {data})  // 谁发给我我就回给谁
+* io.emit('msgg', {data}) // 回给所有监听的人
+
+服务器端监听
+* socket.on('msg', (data) => { console.log(data) })
+
+服务器端获取建立连接的地址
+* socket.request.url
+
+
+
+区分房间
+1. 房间号通过get传值，<br/>
+2. 后台获取到get传值<br/>
+3. 把get传的值 join()<br/>
+服务器端加入房间<br/>
+socket.join(roomid);  roomid为房间的标识<br/>
+
+只给这个房间里的人广播<br/>
+io.to(roomid).emit('msgg', {data})<br/>
+
+通知所有人不包括自己<br/>
+socket.broadcast.to(roomid).emit('msgg', {data})
+
+
+
+
+
+
+
+
+
+
+
--- express_socket/app.js
+++ express_socket/app.js
@@ -10,22 +10,30 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 
+    // 连接socket 
+    io.on('connection', (socket) => {
+
+        console.log('连接了')
+
+        socket.on('to', (data) => {
+
+            console.log(data)
+            
+            socket.emit('go', data)
+        })
+
+        // 监听群发
+        socket.on('toAll', (data) => {
+
+            console.log(data)
+
+            io.emit('go', data)
+        })
+    })
+
     res.render('index')
 })
 
-// 连接socket 
-io.on('connection', (socket) => {
-
-    console.log('连接了')
-
-    // 向客户端发布一个 news
-    socket.emit('news', { hello:'world' });
-
-    // 监听客户端 my other event
-    socket.on('my other event', (data) => {
-        console.log(data)
-    })
-})
 
 server.listen(8000);
 
